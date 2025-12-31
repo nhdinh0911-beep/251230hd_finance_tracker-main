605fdb2c36d1dbc26cae94e7ffec6c182a1682d7
nhdinh0911-beep <nhdinh0911@gmail.com>
2025-12-31 13:49:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # =====================================================
 st.set_page_config(
     page_title="Finance Tracker",
-    page_icon="🤑",
+    page_icon="💰",
     layout="wide"
 )
 
